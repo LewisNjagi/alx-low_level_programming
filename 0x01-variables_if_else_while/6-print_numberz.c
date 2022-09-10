cac864be12c53ae469cbf40786362a479788cddc
LewisNjagi <lewismaimbu@gmail.com>
2022-09-10 06:04:09 -0700

    added 6-print_numbers.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int s;
+
+for (s = 0; s <= 9; s++)
+{
+putchar (s + '0');
+}
+putchar('\n');
+return (0);
+}
