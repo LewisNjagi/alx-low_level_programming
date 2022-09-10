fca611b9cb8249de0f9423aa602c1f7feee8ee82
LewisNjagi <lewismaimbu@gmail.com>
2022-09-09 21:48:46 -0700

    added 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+  
+int main()  
+{  
+char lc = 'a';  
+    
+while(lc <= 'z')  
+{  
+putchar(lc);  
+lc++;  
+}
+putchar('\n');  
+
+return 0;  
+}
