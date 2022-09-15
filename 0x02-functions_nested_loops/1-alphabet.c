9923c6509726ac076f6070f60ee9277203f0b1fa
LewisNjagi <lewismaimbu@gmail.com>
2022-09-15 14:14:39 -0700

    added 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main-entry point for the program.
+ *
+ *
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
