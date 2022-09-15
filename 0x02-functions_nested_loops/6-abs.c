9ebb4d4f433ba6e0dcc10fce1e5fd2b79258575e
LewisNjagi <lewismaimbu@gmail.com>
2022-09-15 16:31:52 -0700

    added 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * _abs - main-entry point for the program.
+ *
+ * @i : character passed as i.
+ *
+ * Return: n
+ */
+
+int _abs(int i)
+{
+return (abs(i));
+}
