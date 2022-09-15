152bab6df41f3c8d5e25f8efe749a525732ea892
LewisNjagi <lewismaimbu@gmail.com>
2022-09-15 16:22:27 -0700

    fixed 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  *
  * @n : character passed as n.
  *
- * Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero, -1 and prints - if n is less than zero.
+ * Return: 1, 0, -1
  */
 
 int print_sign(int n)
