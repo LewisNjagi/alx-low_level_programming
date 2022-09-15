5c1e3a4c6c9a1a02190686ae5a96332913bbe8a0
LewisNjagi <lewismaimbu@gmail.com>
2022-09-15 16:47:00 -0700

    added 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_last_digit - main-entry point for the program.
+ *
+ * @l : character passed as l.
+ *
+ * Return: l
+ */
+
+int print_last_digit(int n)
+{
+int l = n % 10;
+
+if (l < 0)
+l *= -1;
+_putchar(l + '0');
+return (l);
+}
+
