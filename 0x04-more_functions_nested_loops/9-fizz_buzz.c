7868d3891dc97247f232878324025aa4a537d66d
LewisNjagi <lewismaimbu@gmail.com>
2022-09-16 16:34:05 -0700

    fixed 9-fizz_buzz.c file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,36 @@
 
 int main(void)
 {
-int i;
 
-for (i = 1; i <= 100; i++)
+int i = 1;
+
+while (i <= 100)
 {
-if (i == 100)
-printf(" Buzz ");
-else if ((i % 3 == 0) && (i % 5 == 0))
-printf(" FizzBuzz ");
-else if (i % 3 == 0)
-printf(" Fizz ");
-else if (i % 5 == 0)
-printf(" Buzz ");
-else
-printf("%d ", i);
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
 }
-printf("\n");
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%i", i);
+}
+
+if (i != 100)
+{
+putchar(' ');
+}
+
+i++;
+}
+putchar('\n');
 return (0);
+
 }
