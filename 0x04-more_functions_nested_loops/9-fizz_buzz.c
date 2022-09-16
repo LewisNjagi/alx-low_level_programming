9eeba5c88cf7265507bf235ad8a06c109dbfcd16
LewisNjagi <lewismaimbu@gmail.com>
2022-09-16 16:31:12 -0700

    fixed 9-fizz_buzz.c file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints the numbers from 1 to 100
- * 3 multiples print Fizz instead of the number
- * 5 multiples print Buzz instead of the number
- * 3 & 5 multiples print FizzBuzz instead of the number
+ * main - main entry point of the program
+ *
  * Return: 0
  */
 
