dfa7870950e3ef0f940f6e582c3e02610ca63859
LewisNjagi <lewismaimbu@gmail.com>
2022-09-15 18:51:10 -0700

    added 104-fibonacci.c file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+*main - prints out first 98 fibonacci suit numbers
+*
+*Return: 0
+*/
+
+int main(void)
+{
+int i;
+unsigned long f1 = 0, f2 = 1, f3;
+unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
+unsigned long h1, h2;
+
+for (i = 0; i < 92; i++)
+{
+f3 = f1 + f2;
+printf("%lu, ", f3);
+f1 = f2;
+f2 = f3;
+}
+f1_h1 = f1 / 10000000000;
+f2_h1 = f2 / 10000000000;
+f1_h2 = f1 % 10000000000;
+f2_h2 = f2 % 10000000000;
+for (i = 93; i < 99; i++)
+{
+h1 = f1_h1 + f2_h1;
+h2 = f1_h2 + f2_h2;
+if ((f1_h2 + f2_h2) > 9999999999)
+{
+h1 += 1;
+h2 %= 10000000000;
+}
+printf("%lu%lu", h1, h2);
+if (i != 98)
+printf(", ");
+
+f1_h1 = f2_h1;
+f1_h2 = f2_h2;
+f2_h1 = h1;
+f2_h2 = h2;
+}
+printf("\n");
+return (0);
+}
