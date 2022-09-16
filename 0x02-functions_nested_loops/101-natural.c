0c31b0a95e7c45e7c03d6901f81dab7bc2df82a0
LewisNjagi <lewismaimbu@gmail.com>
2022-09-15 18:17:11 -0700

    added 101-natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - check for multiples of 3 and 5
+ *
+ *Return: 0 
+ */
+int main(void)
+{
+int a = 1024;
+int b;
+int s = 0;
+for (b = 0; b < a; b++)
+{
+if ((b % 3 == 0) || (b % 5 == 0))
+{
+s = s + b;
+}
+}
+printf("%d\n", s);
+return (0);
+}
